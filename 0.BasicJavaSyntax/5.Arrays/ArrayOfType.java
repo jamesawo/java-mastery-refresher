091f5b559928782e48bcbc463a2ddb3f0c44c7d0
James Aworo <james.aworo@outlook.com>
2023-03-25 00:04:37 +0100

    add array of different types example

---
+++ 0.BasicJavaSyntax/5.Arrays/ArrayOfType.java
@@ -0,0 +1,14 @@
+public class ArrayOfType {
+    public static void main(String[] args) {
+        byte[] anArrayOfBytes;
+        short[] anArrayOfShorts;
+        long[] anArrayOfLongs;
+        float[] anArrayOfFloats;
+        double[] anArrayOfDoubles;
+        boolean[] anArrayOfBooleans;
+        char[] anArrayOfChars;
+        String[] anArrayOfStrings;
+
+        System.out.println("Creating array of different types");
+    }
+}
