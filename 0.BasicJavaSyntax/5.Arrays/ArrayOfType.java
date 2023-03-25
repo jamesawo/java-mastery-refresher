f903b1e14dfb75202557c10875a4e40f06d7f63f
James Aworo <james.aworo@outlook.com>
2023-03-25 13:32:25 +0100

    update: add explanatory comment

--- 0.BasicJavaSyntax/5.Arrays/ArrayOfType.java
+++ 0.BasicJavaSyntax/5.Arrays/ArrayOfType.java
@@ -1,5 +1,6 @@
 public class ArrayOfType {
     public static void main(String[] args) {
+        // not used, only here to illustrate creating an array
         byte[] anArrayOfBytes;
         short[] anArrayOfShorts;
         long[] anArrayOfLongs;
