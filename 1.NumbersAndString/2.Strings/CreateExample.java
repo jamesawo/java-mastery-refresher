11582f1799c1c4b0fb2a3e9dc17c63b8f04f3ca4
James Aworo <james.aworo@outlook.com>
2023-03-28 13:16:51 +0100

    add creating strings example

---
+++ 1.NumbersAndString/2.Strings/CreateExample.java
@@ -0,0 +1,15 @@
+public class CreateExample {
+    public static void main(String[] args) {
+
+        char[] charArr = { 'h', 'e', 'l', 'l', '0' };
+
+        String aString = new String(charArr);
+        String bString = new String("World");
+        String cString = "Simplest way to create a String";
+
+        System.out.println(aString);
+        System.out.println(bString);
+        System.out.println(cString);
+
+    }
+}
